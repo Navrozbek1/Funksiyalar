15a00d8e03c2923355679de138dd3a8d99842055
Navrozbek <github.navrozbek.352@gmail.com>
2025-12-04 16:22:17 +0500

    funksiyalar
    Bu hali bitmagan ✅

---
+++ Funksiyalar.py
@@ -0,0 +1,31 @@
+# Funksiyaning sintaksisi:
+# def funksiya_nomi(parametr1, parametr2, ...):
+#     funksiya bajarilganda ishlaydigan qodi yozing
+#     return qaytadigan qiymat
+
+# def salomlashish(ism, familiya):
+#     matn = f"Nimagapla {ism} {familiya} darsimizga hush kelibsan !"
+#     return matn
+
+# ism = input("Ismingizni kiriting: ").title()
+# fam = input("Familiyangizni kiriting: ").title()
+
+# print(salomlashish(ism, fam))
+
+""" Bu yerda mukammal qanqulator def yordamida yaratilgan !"""
+
+# def kankulator(x, y, belgi):
+#     if belgi == '+':
+#         return f"Yig'indi: {x + y}"
+#     elif belgi == '-':
+#         return f"Ayirma: {x - y}"
+#     elif belgi == '*':
+#         return f"Ko'paytma: {x * y}"
+#     elif belgi == '/':
+#         return f"Bo'linma: {x / y}"
+#     elif belgi == '**':
+#         return f"Daraja: {x ** y}"
+#     elif belgi == '//':
+#         return f"Qoldiqsiz bo'lish: {x // y}"
+#     elif belgi == '1/2':
+#         return f"Yig'indi: {x + y}"
